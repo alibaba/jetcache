f3c9fb06ed32354bf91a1ffcb582143fcc9564ab
huangli <areyouok@gmail.com>
2013-09-14 17:54:21 +0800

    spring

---
+++ cache-spring/src/main/java/com/taobao/geek/cache/spring/CacheFactoryBean.java
@@ -0,0 +1,47 @@
+/**
+ * Created on  13-09-14 17:32
+ */
+package com.taobao.geek.cache.spring;
+
+import com.taobao.geek.cache.CacheClient;
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * @author yeli.hl
+ */
+public class CacheFactoryBean implements FactoryBean {
+
+    private Object target;
+    private CacheClient cacheClient;
+
+    @Override
+    public Object getObject() throws Exception {
+        return cacheClient.getProxyByAnnotation(target);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return null;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public Object getTarget() {
+        return target;
+    }
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public CacheClient getCacheClient() {
+        return cacheClient;
+    }
+
+    public void setCacheClient(CacheClient cacheClient) {
+        this.cacheClient = cacheClient;
+    }
+}
