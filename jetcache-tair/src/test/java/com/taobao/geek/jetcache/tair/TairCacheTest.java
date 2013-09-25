9044b3d58825b4dc8d7f3e761bcb6be7ccafc04d
huangli <areyouok@gmail.com>
2013-09-25 11:13:34 +0800

    优化导入

--- jetcache-tair/src/test/java/com/taobao/geek/jetcache/tair/TairCacheTest.java
+++ jetcache-tair/src/test/java/com/taobao/geek/jetcache/tair/TairCacheTest.java
@@ -3,8 +3,6 @@
  */
 package com.taobao.geek.jetcache.tair;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.taobao.geek.jetcache.CacheConfig;
 import com.taobao.geek.jetcache.CacheResult;
 import com.taobao.geek.jetcache.CacheResultCode;
