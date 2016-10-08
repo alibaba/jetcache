89013f7e28b4d5ac58f316aae2b1383451909c09
huangli <areyouok@gmail.com>
2016-10-08 16:24:18 +0800

    CompoundCacheTest

---
+++ jetcache-test/src/test/java/com/alicp/jetcache/CompoundCacheTest.java
@@ -0,0 +1,38 @@
+package com.alicp.jetcache;
+
+import com.alicp.jetcache.embedded.EmbeddedCacheBuilder;
+import com.alicp.jetcache.embedded.EmbeddedCacheConfig;
+import com.alicp.jetcache.embedded.LinkedHashMapCache;
+import com.alicp.jetcache.support.FastjsonKeyConvertor;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created on 2016/10/8.
+ *
+ * @author <a href="mailto:yeli.hl@taobao.com">huangli</a>
+ */
+public class CompoundCacheTest extends AbstractCacheTest {
+
+    @Test
+    public void test() throws Exception {
+        Cache<Object, Object> l1Cache = EmbeddedCacheBuilder.createEmbeddedCacheBuilder()
+                .limit(10)
+                .defaultExpire(200, TimeUnit.MILLISECONDS)
+                .keyConvertor(FastjsonKeyConvertor.INSTANCE)
+                .expireAfterWrite()
+                .buildFunc(c -> new LinkedHashMapCache((EmbeddedCacheConfig) c))
+                .build();
+        Cache<Object, Object> l2Cache = EmbeddedCacheBuilder.createEmbeddedCacheBuilder()
+                .limit(100000)
+                .defaultExpire(200, TimeUnit.MILLISECONDS)
+                .keyConvertor(FastjsonKeyConvertor.INSTANCE)
+                .softValues()
+                .expireAfterWrite()
+                .buildFunc(c -> new LinkedHashMapCache((EmbeddedCacheConfig) c))
+                .build();
+        cache = new CompoundCache<>(l1Cache, l2Cache);
+        baseTest();
+    }
+}
