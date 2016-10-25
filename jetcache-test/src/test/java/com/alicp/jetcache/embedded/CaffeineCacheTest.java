86adc6fa0ec1aba57d2d3e17255c512cab24ea24
huangli <areyouok@gmail.com>
2016-10-25 17:39:05 +0800

    test

--- jetcache-test/src/test/java/com/alicp/jetcache/embedded/CaffeineCacheTest.java
+++ jetcache-test/src/test/java/com/alicp/jetcache/embedded/CaffeineCacheTest.java
@@ -2,8 +2,11 @@ package com.alicp.jetcache.embedded;
 
 import com.alicp.jetcache.Cache;
 import com.alicp.jetcache.CacheConfig;
+import com.alicp.jetcache.CacheResultCode;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 /**
@@ -20,6 +23,18 @@ public class CaffeineCacheTest extends AbstractEmbeddedCacheTest {
     @Test
     public void test() throws Exception {
         super.test(false);
+
+//        cache = EmbeddedCacheBuilder.createEmbeddedCacheBuilder()
+//                .buildFunc(getBuildFunc()).expireAfterWrite(100, TimeUnit.MILLISECONDS).limit(2).build();
+//        cache.put("K1", "V1");
+//        cache.put("K2", "V2");
+//        cache.put("K3", "V3");
+//        System.out.println(cache.get("K1"));
+//        System.out.println(cache.get("K2"));
+//        System.out.println(cache.get("K3"));
+//        System.out.println(cache.get("K1"));
+//        System.out.println(cache.get("K2"));
+//        System.out.println(cache.get("K3"));
     }
 
 }
