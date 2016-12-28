d7b99f613a515f89f7346d02cecc4bc6f0758cfa
huangli <areyouok@gmail.com>
2016-12-28 10:30:50 +0800

    处理空值

--- jetcache-core/src/main/java/com/alicp/jetcache/support/FastjsonKeyConvertor.java
+++ jetcache-core/src/main/java/com/alicp/jetcache/support/FastjsonKeyConvertor.java
@@ -15,6 +15,9 @@ public class FastjsonKeyConvertor implements Function<Object, Object> {
     public static final FastjsonKeyConvertor INSTANCE = new FastjsonKeyConvertor();
 
     public Object apply(Object originalKey) {
+        if (originalKey == null) {
+            return null;
+        }
         if (originalKey instanceof String) {
             return originalKey;
         }
