b09cbeb0e7f9e891b7b3a77edecf6c908a4c03ed
huangli <areyouok@gmail.com>
2016-12-30 17:03:44 +0800

    fastjson key convertor不处理byte[]

--- jetcache-core/src/main/java/com/alicp/jetcache/support/FastjsonKeyConvertor.java
+++ jetcache-core/src/main/java/com/alicp/jetcache/support/FastjsonKeyConvertor.java
@@ -21,9 +21,6 @@ public class FastjsonKeyConvertor implements Function<Object, Object> {
         if (originalKey instanceof String) {
             return originalKey;
         }
-        if (originalKey instanceof byte[]) {
-            return originalKey;
-        }
         return JSON.toJSONString(originalKey);
     }
 
