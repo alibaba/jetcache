fbc1cee90b2c333109ed6687e9a38145df10d7ab
huangli <areyouok@gmail.com>
2017-01-12 11:07:48 +0800

    deprecated

--- jetcache-anno-api/src/main/java/com/alicp/jetcache/anno/SerialPolicy.java
+++ jetcache-anno-api/src/main/java/com/alicp/jetcache/anno/SerialPolicy.java
@@ -8,6 +8,9 @@ package com.alicp.jetcache.anno;
  */
 public interface SerialPolicy {
     String JAVA = "JAVA";
+
+    @Deprecated
     String FASTJSON = "FASTJSON";
+
     String KRYO = "KRYO";
 }
