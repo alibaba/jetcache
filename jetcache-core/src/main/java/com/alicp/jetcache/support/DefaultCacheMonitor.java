331a20fff71ae31307abf864b2e12a56e507f263
huangli <areyouok@gmail.com>
2017-02-23 14:39:53 +0800

    修改DefaultCacheMonitor

--- jetcache-core/src/main/java/com/alicp/jetcache/support/DefaultCacheMonitor.java
+++ jetcache-core/src/main/java/com/alicp/jetcache/support/DefaultCacheMonitor.java
@@ -1,10 +1,12 @@
 package com.alicp.jetcache.support;
 
-import com.alicp.jetcache.CacheGetResult;
-import com.alicp.jetcache.CacheMonitor;
-import com.alicp.jetcache.CacheResult;
+import com.alicp.jetcache.*;
+import com.alicp.jetcache.event.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created on 2016/10/27.
@@ -43,11 +45,40 @@ public class DefaultCacheMonitor implements CacheMonitor {
     }
 
     @Override
-    public synchronized void afterGET(long millis, Object key, CacheGetResult result) {
+    public synchronized void afterOperation(CacheEvent event) {
+        if (event instanceof CacheGetEvent) {
+            CacheGetEvent e = (CacheGetEvent) event;
+            afterGet(e.getMillis(), e.getKey(), e.getResult());
+        } else if (event instanceof CachePutEvent) {
+            CachePutEvent e = (CachePutEvent) event;
+            afterPut(e.getMillis(), e.getKey(), e.getValue(), e.getResult());
+        } else if (event instanceof CacheRemoveEvent) {
+            CacheRemoveEvent e = (CacheRemoveEvent) event;
+            afterRemove(e.getMillis(), e.getKey(), e.getResult());
+        } else if (event instanceof CacheLoadEvent) {
+            CacheLoadEvent e = (CacheLoadEvent) event;
+            afterLoad(e.getMillis(), e.getKey(), e.getLoadedValue(), e.isSuccess());
+        } else if (event instanceof CacheGetAllEvent) {
+            CacheGetAllEvent e = (CacheGetAllEvent) event;
+            afterGetAll(e.getMillis(), e.getKeys(), e.getResult());
+        } else if (event instanceof CachePutAllEvent) {
+            CachePutAllEvent e = (CachePutAllEvent) event;
+            afterPutAll(e.getMillis(), e.getMap(), e.getResult());
+        } else if (event instanceof CacheRemoveAllEvent) {
+            CacheRemoveAllEvent e = (CacheRemoveAllEvent) event;
+            afterRemoveAll(e.getMillis(), e.getKeys(), e.getResult());
+        }
+    }
+
+    private void afterGet(long millis, Object key, CacheGetResult result) {
         cacheStat.minGetTime = Math.min(cacheStat.minGetTime, millis);
         cacheStat.maxGetTime = Math.max(cacheStat.maxGetTime, millis);
         cacheStat.getTimeSum += millis;
         cacheStat.getCount++;
+        parseSingleGet(result);
+    }
+
+    private void parseSingleGet(CacheGetResult result) {
         switch (result.getResultCode()) {
             case SUCCESS:
                 cacheStat.getHitCount++;
@@ -61,11 +92,12 @@ public class DefaultCacheMonitor implements CacheMonitor {
             case FAIL:
                 cacheStat.getFailCount++;
                 break;
+            default:
+                logger.warn("jetcache get return unexpected code: " + result.getResultCode());
         }
     }
 
-    @Override
-    public synchronized void afterPUT(long millis, Object key, Object value, CacheResult result) {
+    private void afterPut(long millis, Object key, Object value, CacheResult result) {
         cacheStat.minPutTime = Math.min(cacheStat.minPutTime, millis);
         cacheStat.maxPutTime = Math.max(cacheStat.maxPutTime, millis);
         cacheStat.putTimeSum += millis;
@@ -84,8 +116,7 @@ public class DefaultCacheMonitor implements CacheMonitor {
         }
     }
 
-    @Override
-    public synchronized void afterREMOVE(long millis, Object key, CacheResult result) {
+    private void afterRemove(long millis, Object key, CacheResult result) {
         cacheStat.minInvalidateTime = Math.min(cacheStat.minInvalidateTime, millis);
         cacheStat.maxInvalidateTime = Math.max(cacheStat.maxInvalidateTime, millis);
         cacheStat.invalidateTimeSum += millis;
@@ -102,8 +133,7 @@ public class DefaultCacheMonitor implements CacheMonitor {
         }
     }
 
-    @Override
-    public synchronized void afterLoad(long millis, Object key, Object loadedValue, boolean success) {
+    private void afterLoad(long millis, Object key, Object loadedValue, boolean success) {
         cacheStat.minLoadTime = Math.min(cacheStat.minLoadTime, millis);
         cacheStat.maxLoadTime = Math.max(cacheStat.maxLoadTime, millis);
         cacheStat.loadTimeSum += millis;
@@ -114,4 +144,57 @@ public class DefaultCacheMonitor implements CacheMonitor {
             cacheStat.loadFailCount++;
         }
     }
+
+    private void afterGetAll(long millis, Set keys, MultiGetResult result) {
+        if (keys == null) {
+            return;
+        }
+        int keyCount = keys.size();
+        cacheStat.minGetTime = Math.min(cacheStat.minGetTime, millis);
+        cacheStat.maxGetTime = Math.max(cacheStat.maxGetTime, millis);
+        cacheStat.getTimeSum += millis;
+        cacheStat.getCount += keyCount;
+        Map resultValues = result.getValues();
+        if (resultValues == null) {
+            cacheStat.getFailCount += keyCount;
+        } else {
+            for (Object singleResult : resultValues.values()) {
+                CacheGetResult r = ((CacheGetResult) singleResult);
+                parseSingleGet(r);
+            }
+        }
+    }
+
+    private void afterRemoveAll(long millis, Set keys, CacheResult result) {
+        if (keys == null) {
+            return;
+        }
+        int keyCount = keys.size();
+        cacheStat.minInvalidateTime = Math.min(cacheStat.minInvalidateTime, millis);
+        cacheStat.maxInvalidateTime = Math.max(cacheStat.maxInvalidateTime, millis);
+        cacheStat.invalidateTimeSum += millis;
+        cacheStat.invalidateCount += keyCount;
+        if (result.isSuccess()) {
+            cacheStat.invalidateSuccessCount += keyCount;
+        } else {
+            cacheStat.invalidateFailCount += keyCount;
+        }
+    }
+
+    private void afterPutAll(long millis, Map map, CacheResult result) {
+        if (map == null) {
+            return;
+        }
+        int keyCount = map.size();
+        cacheStat.minPutTime = Math.min(cacheStat.minPutTime, millis);
+        cacheStat.maxPutTime = Math.max(cacheStat.maxPutTime, millis);
+        cacheStat.putTimeSum += millis;
+        cacheStat.putCount += keyCount;
+        if (result.isSuccess()) {
+            cacheStat.putSuccessCount += keyCount;
+        } else {
+            cacheStat.putFailCount += keyCount;
+        }
+    }
+
 }
