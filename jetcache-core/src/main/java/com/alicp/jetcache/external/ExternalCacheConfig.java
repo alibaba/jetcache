a5409f416ae03aa3b8d2d1af91f13d15d7d09bb9
DarknessTM <darknesstm@gmail.com>
2021-08-21 20:36:47 +0800

    cache key prefix can dynamic change in runtime (#593)

--- jetcache-core/src/main/java/com/alicp/jetcache/external/ExternalCacheConfig.java
+++ jetcache-core/src/main/java/com/alicp/jetcache/external/ExternalCacheConfig.java
@@ -5,6 +5,7 @@ import com.alicp.jetcache.support.DecoderMap;
 import com.alicp.jetcache.support.JavaValueEncoder;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Created on 16/9/9.
@@ -12,16 +13,32 @@ import java.util.function.Function;
  * @author <a href="mailto:areyouok@gmail.com">huangli</a>
  */
 public class ExternalCacheConfig<K, V> extends CacheConfig<K, V> {
+    @Deprecated
     private String keyPrefix;
+
+    private Supplier<String> keyPrefixSupplier;
     private Function<Object, byte[]> valueEncoder = JavaValueEncoder.INSTANCE;
     private Function<byte[], Object> valueDecoder = DecoderMap.defaultJavaValueDecoder();
 
     public String getKeyPrefix() {
+        // keyPrefix is higher priority.
+        if (keyPrefix == null && keyPrefixSupplier != null) {
+            return keyPrefixSupplier.get();
+        }
         return keyPrefix;
     }
 
+    @Deprecated
     public void setKeyPrefix(String keyPrefix) {
         this.keyPrefix = keyPrefix;
+    }
+
+    public Supplier<String> getKeyPrefixSupplier() {
+        return keyPrefixSupplier;
+    }
+
+    public void setKeyPrefixSupplier(Supplier<String> keyPrefixSupplier) {
+        this.keyPrefixSupplier = keyPrefixSupplier;
     }
 
     public Function<Object, byte[]> getValueEncoder() {
