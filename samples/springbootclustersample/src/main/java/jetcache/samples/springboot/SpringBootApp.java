3c80299b299944b6ed0865e0b9c039fc82c5d8b6
etcZYP <972169092@qq.com>
2022-03-22 06:46:28 +0800

    对springboot项目集群环境进行拓展，支持多节点同步失效本地缓存

---
+++ samples/springbootclustersample/src/main/java/jetcache/samples/springboot/SpringBootApp.java
@@ -0,0 +1,18 @@
+/**
+ * Created on 2018/8/11.
+ */
+package jetcache.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:areyouok@gmail.com">huangli</a>
+ */
+@SpringBootApplication
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+}
