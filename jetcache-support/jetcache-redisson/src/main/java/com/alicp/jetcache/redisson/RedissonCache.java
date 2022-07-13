0e28c910da49bbf3afd340a4c35e1b6dcb770c46
杨勇 <jeason1914@gmail.com>
2022-07-13 14:55:53 +0800

    fix: 修改redisson依赖及实现，添加单元测试代码

--- jetcache-support/jetcache-redisson/src/main/java/com/alicp/jetcache/redisson/RedissonCache.java
+++ jetcache-support/jetcache-redisson/src/main/java/com/alicp/jetcache/redisson/RedissonCache.java
@@ -5,10 +5,6 @@ import com.alicp.jetcache.external.AbstractExternalCache;
 import org.redisson.api.RBatch;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.util.Pair;
-import org.springframework.util.CollectionUtils;
 
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -16,7 +12,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 /**
  * Created on 2022/7/12.
@@ -24,7 +19,6 @@ import java.util.stream.Collectors;
  * @author <a href="mailto:jeason1914@qq.com">yangyong</a>
  */
 public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
-    private final Logger logger = LoggerFactory.getLogger(RedissonCache.class);
     private final RedissonClient client;
     private final RedissonCacheConfig<K, V> config;
 
@@ -56,14 +50,15 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
             final RBucket<CacheValueHolder<V>> rb = this.client.getBucket(getCacheKey(key));
             final CacheValueHolder<V> holder = rb.get();
             if (Objects.nonNull(holder)) {
-                if (System.currentTimeMillis() >= holder.getExpireTime()) {
+                final long now = System.currentTimeMillis(), expire = holder.getExpireTime();
+                if (expire > 0 && now >= expire) {
                     return CacheGetResult.EXPIRED_WITHOUT_MSG;
                 }
                 return new CacheGetResult<>(CacheResultCode.SUCCESS, null, holder);
             }
             return CacheGetResult.NOT_EXISTS_WITHOUT_MSG;
         } catch (Throwable e) {
-            logger.warn("do_GET(key: {})-exp: {}", key, e.getMessage());
+            logError("GET", key, e);
             return new CacheGetResult<>(e);
         }
     }
@@ -72,70 +67,109 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
     @SuppressWarnings({"unchecked"})
     protected MultiGetResult<K, V> do_GET_ALL(final Set<? extends K> keys) {
         try {
-            final Map<K, CacheGetResult<V>> retMap = CollectionUtils.isEmpty(keys) ? new HashMap<>(0) :
-                    keys.stream()
-                            .map(key -> {
-                                final RBucket<CacheValueHolder<V>> rb = this.client.getBucket(getCacheKey(key));
-                                final CacheValueHolder<V> holder = rb.get();
-                                if (Objects.nonNull(holder)) {
-                                    if (System.currentTimeMillis() >= holder.getExpireTime()) {
-                                        return Pair.of(key, CacheGetResult.EXPIRED_WITHOUT_MSG);
-                                    }
-                                    return Pair.of(key, new CacheGetResult<>(CacheResultCode.SUCCESS, null, holder));
-                                }
-                                return Pair.of(key, CacheGetResult.NOT_EXISTS_WITHOUT_MSG);
-                            })
-                            .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond, (n, o) -> n));
+            final Map<K, CacheGetResult<V>> retMap = new HashMap<>(1 << 4);
+            if (Objects.nonNull(keys) && !keys.isEmpty()) {
+                final Map<K, String> keyMap = new HashMap<>(keys.size());
+                for (K k : keys) {
+                    if (Objects.nonNull(k)) {
+                        final String key = getCacheKey(k);
+                        if (Objects.nonNull(key) && !key.isEmpty()) {
+                            keyMap.put(k, key);
+                        }
+                    }
+                }
+                if (!keyMap.isEmpty()) {
+                    final Map<String, Object> kvMap = this.client.getBuckets().get(keyMap.values().toArray(new String[0]));
+                    final long now = System.currentTimeMillis();
+                    for (K k : keys) {
+                        final String key = keyMap.get(k);
+                        if (Objects.nonNull(key) && Objects.nonNull(kvMap) && !key.isEmpty()) {
+                            final CacheValueHolder<V> holder = (CacheValueHolder<V>) kvMap.get(key);
+                            if (Objects.nonNull(holder)) {
+                                final long expire = holder.getExpireTime();
+                                final CacheGetResult<V> ret = (expire > 0 && now >= expire) ? CacheGetResult.EXPIRED_WITHOUT_MSG :
+                                        new CacheGetResult<>(CacheResultCode.SUCCESS, null, holder);
+                                retMap.put(k, ret);
+                                continue;
+                            }
+                        }
+                        retMap.put(k, CacheGetResult.NOT_EXISTS_WITHOUT_MSG);
+                    }
+                }
+            }
             return new MultiGetResult<>(CacheResultCode.SUCCESS, null, retMap);
         } catch (Throwable e) {
-            logger.warn("do_GET_ALL(keys: {})-exp: {}", keys, e.getMessage());
+            logError("GET_ALL", "keys(" + (Objects.nonNull(keys) ? keys.size() : 0) + ")", e);
             return new MultiGetResult<>(e);
         }
     }
 
     @Override
     protected CacheResult do_PUT(final K key, final V value, final long expireAfterWrite, final TimeUnit timeUnit) {
-        final RBucket<CacheValueHolder<V>> rb = this.client.getBucket(getCacheKey(key));
-        final CacheValueHolder<V> holder = new CacheValueHolder<>(value, timeUnit.toMillis(expireAfterWrite));
-        rb.set(holder, expireAfterWrite, TimeUnit.MILLISECONDS);
-        return CacheGetResult.SUCCESS_WITHOUT_MSG;
+        try {
+            final CacheValueHolder<V> holder = new CacheValueHolder<>(value, timeUnit.toMillis(expireAfterWrite));
+            this.client.getBucket(getCacheKey(key)).set(holder);
+            return CacheGetResult.SUCCESS_WITHOUT_MSG;
+        } catch (Throwable e) {
+            logError("PUT", key, e);
+            return new CacheResult(e);
+        }
     }
 
     @Override
     protected CacheResult do_PUT_ALL(final Map<? extends K, ? extends V> map, final long expireAfterWrite, final TimeUnit timeUnit) {
-        if (!CollectionUtils.isEmpty(map)) {
-            final RBatch batch = this.client.createBatch();
-            map.forEach((k, v) -> {
-                final CacheValueHolder<V> holder = new CacheValueHolder<>(v, timeUnit.toMillis(expireAfterWrite));
-                batch.getBucket(getCacheKey(k)).setAsync(holder, expireAfterWrite, TimeUnit.MILLISECONDS);
-            });
-            batch.execute();
+        try {
+            if (Objects.nonNull(map) && !map.isEmpty()) {
+                final long expire = timeUnit.toMillis(expireAfterWrite);
+                final RBatch batch = this.client.createBatch();
+                map.forEach((k, v) -> {
+                    final CacheValueHolder<V> holder = new CacheValueHolder<>(v, expire);
+                    batch.getBucket(getCacheKey(k)).setAsync(holder);
+                });
+                batch.execute();
+            }
+            return CacheResult.SUCCESS_WITHOUT_MSG;
+        } catch (Throwable e) {
+            logError("PUT_ALL", "map(" + map.size() + ")", e);
+            return new CacheResult(e);
         }
-        return CacheResult.SUCCESS_WITHOUT_MSG;
     }
 
     @Override
     protected CacheResult do_REMOVE(final K key) {
-        final RBucket<CacheValueHolder<V>> rb = this.client.getBucket(getCacheKey(key));
-        rb.delete();
-        return CacheResult.SUCCESS_WITHOUT_MSG;
+        try {
+            final boolean ret = this.client.getBucket(getCacheKey(key)).delete();
+            return ret ? CacheResult.SUCCESS_WITHOUT_MSG : CacheResult.FAIL_WITHOUT_MSG;
+        } catch (Throwable e) {
+            logError("REMOVE", key, e);
+            return new CacheResult(e);
+        }
     }
 
     @Override
     protected CacheResult do_REMOVE_ALL(final Set<? extends K> keys) {
-        if (!CollectionUtils.isEmpty(keys)) {
-            final RBatch batch = this.client.createBatch();
-            keys.forEach(key -> batch.getBucket(getCacheKey(key)).deleteAsync());
-            batch.execute();
+        try {
+            if (Objects.nonNull(keys) && !keys.isEmpty()) {
+                final RBatch batch = this.client.createBatch();
+                keys.forEach(key -> batch.getBucket(getCacheKey(key)).deleteAsync());
+                batch.execute();
+            }
+            return CacheResult.SUCCESS_WITHOUT_MSG;
+        } catch (Throwable e) {
+            logError("REMOVE_ALL", "keys(" + keys.size() + ")", e);
+            return new CacheResult(e);
         }
-        return CacheResult.SUCCESS_WITHOUT_MSG;
     }
 
     @Override
     protected CacheResult do_PUT_IF_ABSENT(final K key, final V value, final long expireAfterWrite, final TimeUnit timeUnit) {
-        final CacheValueHolder<V> holder = new CacheValueHolder<>(value, timeUnit.toMillis(expireAfterWrite));
-        final RBucket<CacheValueHolder<V>> rb = this.client.getBucket(getCacheKey(key));
-        final boolean success = rb.trySet(holder, expireAfterWrite, TimeUnit.MILLISECONDS);
-        return success ? CacheResult.SUCCESS_WITHOUT_MSG : CacheResult.EXISTS_WITHOUT_MSG;
+        try {
+            final CacheValueHolder<V> holder = new CacheValueHolder<>(value, timeUnit.toMillis(expireAfterWrite));
+            final boolean success = this.client.getBucket(getCacheKey(key)).trySet(holder, expireAfterWrite, timeUnit);
+            return success ? CacheResult.SUCCESS_WITHOUT_MSG : CacheResult.EXISTS_WITHOUT_MSG;
+        } catch (Throwable e) {
+            logError("PUT_IF_ABSENT", key, e);
+            return new CacheResult(e);
+        }
     }
 }
---
+++ jetcache-test/src/test/java/com/alicp/jetcache/autoconfigure/RedissonStarterTest.java
@@ -0,0 +1,56 @@
+package com.alicp.jetcache.autoconfigure;
+
+import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.anno.CreateCache;
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
+import com.alicp.jetcache.test.beans.MyFactoryBean;
+import com.alicp.jetcache.test.spring.SpringTest;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created on 2022/7/13.
+ *
+ * @author <a href="mailto:jeason1914@qq.com">yangyong</a>
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.alicp.jetcache.test.beans", "com.alicp.jetcache.anno.inittestbeans"})
+@EnableMethodCache(basePackages = {"com.alicp.jetcache.test.beans", "com.alicp.jetcache.anno.inittestbeans"})
+@EnableCreateCacheAnnotation
+@Ignore
+public class RedissonStarterTest extends SpringTest {
+
+    @Test
+    public void tests() throws Exception {
+        System.setProperty("spring.profiles.active", "redisson");
+        context = SpringApplication.run(RedissonStarterTest.class);
+        doTest();
+    }
+
+    @Component
+    public static class A {
+        @CreateCache
+        private Cache cache;
+
+        @PostConstruct
+        public void test() {
+            Assert.assertTrue(cache.PUT("K", "V").isSuccess());
+        }
+    }
+
+    @Bean(name = "factoryBeanTarget")
+    public MyFactoryBean factoryBean() {
+        return new MyFactoryBean();
+    }
+}
