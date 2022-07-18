a98500b53d0fde05636631f528ca85665457fd60
huangli <areyouok@gmail.com>
2022-07-18 16:47:14 +0800

    add some simple samples: JedisExample/LettuceExample

---
+++ samples/simple-samples/src/main/java/LettuceExample.java
@@ -0,0 +1,22 @@
+/**
+ * Created on 2022/07/18.
+ */
+
+import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.redis.lettuce.RedisLettuceCacheBuilder;
+import io.lettuce.core.RedisClient;
+
+/**
+ * @author <a href="mailto:areyouok@gmail.com">huangli</a>
+ */
+public class LettuceExample {
+    public static void main(String[] args) {
+        Cache<String, String> cache = RedisLettuceCacheBuilder.createRedisLettuceCacheBuilder()
+                .redisClient(RedisClient.create("redis://127.0.0.1"))
+                .keyPrefix("myCachePrefix")
+                .buildCache();
+        cache.put("K1", "V1");
+        System.out.println(cache.get("K1"));
+    }
+
+}
