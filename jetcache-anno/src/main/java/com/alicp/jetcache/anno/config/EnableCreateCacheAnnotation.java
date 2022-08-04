62bc8d3982107a9470dd7d4ff74123fda112eb40
huangli <areyouok@gmail.com>
2022-08-04 21:26:46 +0800

    deprecate @EnableCreateCacheAnnotation

--- jetcache-anno/src/main/java/com/alicp/jetcache/anno/config/EnableCreateCacheAnnotation.java
+++ jetcache-anno/src/main/java/com/alicp/jetcache/anno/config/EnableCreateCacheAnnotation.java
@@ -3,17 +3,25 @@ package com.alicp.jetcache.anno.config;
 /**
  * Created on 2016/12/13.
  *
+ * @deprecated CreateCache annotation is replaced by CacheManager.getOrCreateCache(QuickConfig), the CacheManager instance
+ *             can be injected use annotation such as @Autowired.
+ *
  * @author <a href="mailto:areyouok@gmail.com">huangli</a>
  */
 
 import com.alicp.jetcache.anno.field.CreateCacheAnnotationBeanPostProcessor;
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Import({CommonConfiguration.class, CreateCacheAnnotationBeanPostProcessor.class})
+@Deprecated
 public @interface EnableCreateCacheAnnotation {
 }
