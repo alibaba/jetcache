53ca4cec03a98587a13c641286c4aaf0b9d1bf72
jeasonyoung <jeason1914@qq.com>
2022-12-05 15:41:43 +0800

    fix: 解决jetcache、redisson序列化方式一致性问题

--- jetcache-support/jetcache-redisson/src/main/java/com/alicp/jetcache/redisson/RedissonCache.java
+++ jetcache-support/jetcache-redisson/src/main/java/com/alicp/jetcache/redisson/RedissonCache.java
@@ -1,22 +1,23 @@
 package com.alicp.jetcache.redisson;
 
-import com.alicp.jetcache.CacheConfig;
-import com.alicp.jetcache.CacheGetResult;
-import com.alicp.jetcache.CacheResult;
-import com.alicp.jetcache.CacheResultCode;
-import com.alicp.jetcache.CacheValueHolder;
-import com.alicp.jetcache.MultiGetResult;
+import com.alicp.jetcache.*;
 import com.alicp.jetcache.external.AbstractExternalCache;
 import org.redisson.api.RBatch;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.ByteArrayCodec;
+import org.redisson.client.codec.Codec;
+import org.redisson.codec.KryoCodec;
+import org.redisson.codec.SnappyCodecV2;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 /**
  * Created on 2022/7/12.
@@ -26,11 +27,15 @@ import java.util.concurrent.TimeUnit;
 public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
     private final RedissonClient client;
     private final RedissonCacheConfig<K, V> config;
+    private Function<Object, byte[]> valueEncoder;
+    private Function<byte[], Object> valueDecoder;
 
     public RedissonCache(final RedissonCacheConfig<K, V> config) {
         super(config);
         this.config = config;
         this.client = config.getRedissonClient();
+        this.valueEncoder = config.getValueEncoder();
+        this.valueDecoder = config.getValueDecoder();
     }
 
     protected String getCacheKey(final K key) {
@@ -48,12 +53,36 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
         throw new UnsupportedOperationException("RedissonCache does not support unwrap");
     }
 
+    private Codec getCodec() {
+        return ByteArrayCodec.INSTANCE;
+    }
+
+    private byte[] encoder(final CacheValueHolder<V> holder) {
+        if (Objects.nonNull(holder)) {
+            return valueEncoder.apply(holder);
+        }
+        return null;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private CacheValueHolder<V> decoder(final K key, final byte[] data) {
+        CacheValueHolder<V> holder = null;
+        if (data != null && data.length > 0) {
+            try {
+                holder = (CacheValueHolder<V>) valueDecoder.apply(data);
+            } catch (Throwable e) {
+                logError("decoder", key, e);
+            }
+        }
+        return holder;
+    }
+
     @Override
     @SuppressWarnings({"unchecked"})
     protected CacheGetResult<V> do_GET(final K key) {
         try {
-            final RBucket<CacheValueHolder<V>> rb = this.client.getBucket(getCacheKey(key));
-            final CacheValueHolder<V> holder = rb.get();
+            final RBucket<byte[]> rb = this.client.getBucket(getCacheKey(key), getCodec());
+            final CacheValueHolder<V> holder = decoder(key, rb.get());
             if (Objects.nonNull(holder)) {
                 final long now = System.currentTimeMillis(), expire = holder.getExpireTime();
                 if (expire > 0 && now >= expire) {
@@ -75,21 +104,19 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
             final Map<K, CacheGetResult<V>> retMap = new HashMap<>(1 << 4);
             if (Objects.nonNull(keys) && !keys.isEmpty()) {
                 final Map<K, String> keyMap = new HashMap<>(keys.size());
-                for (K k : keys) {
-                    if (Objects.nonNull(k)) {
-                        final String key = getCacheKey(k);
-                        if (Objects.nonNull(key)) {
-                            keyMap.put(k, key);
-                        }
+                keys.stream().filter(Objects::nonNull).forEach(k -> {
+                    final String key = getCacheKey(k);
+                    if (Objects.nonNull(key)) {
+                        keyMap.put(k, key);
                     }
-                }
+                });
                 if (!keyMap.isEmpty()) {
-                    final Map<String, Object> kvMap = this.client.getBuckets().get(keyMap.values().toArray(new String[0]));
+                    final Map<String, byte[]> kvMap = this.client.getBuckets(getCodec()).get(keyMap.values().toArray(new String[0]));
                     final long now = System.currentTimeMillis();
                     for (K k : keys) {
                         final String key = keyMap.get(k);
                         if (Objects.nonNull(key) && Objects.nonNull(kvMap)) {
-                            final CacheValueHolder<V> holder = (CacheValueHolder<V>) kvMap.get(key);
+                            final CacheValueHolder<V> holder = decoder(k, kvMap.get(key));
                             if (Objects.nonNull(holder)) {
                                 final long expire = holder.getExpireTime();
                                 final CacheGetResult<V> ret = (expire > 0 && now >= expire) ? CacheGetResult.EXPIRED_WITHOUT_MSG :
@@ -113,7 +140,7 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
     protected CacheResult do_PUT(final K key, final V value, final long expireAfterWrite, final TimeUnit timeUnit) {
         try {
             final CacheValueHolder<V> holder = new CacheValueHolder<>(value, timeUnit.toMillis(expireAfterWrite));
-            this.client.getBucket(getCacheKey(key)).set(holder, expireAfterWrite, timeUnit);
+            this.client.getBucket(getCacheKey(key), getCodec()).set(encoder(holder), expireAfterWrite, timeUnit);
             return CacheGetResult.SUCCESS_WITHOUT_MSG;
         } catch (Throwable e) {
             logError("PUT", key, e);
@@ -129,7 +156,7 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
                 final RBatch batch = this.client.createBatch();
                 map.forEach((k, v) -> {
                     final CacheValueHolder<V> holder = new CacheValueHolder<>(v, expire);
-                    batch.getBucket(getCacheKey(k)).setAsync(holder, expireAfterWrite, timeUnit);
+                    batch.getBucket(getCacheKey(k), getCodec()).setAsync(encoder(holder), expireAfterWrite, timeUnit);
                 });
                 batch.execute();
             }
@@ -143,7 +170,7 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
     @Override
     protected CacheResult do_REMOVE(final K key) {
         try {
-            final boolean ret = this.client.getBucket(getCacheKey(key)).delete();
+            final boolean ret = this.client.getBucket(getCacheKey(key), getCodec()).delete();
             return ret ? CacheResult.SUCCESS_WITHOUT_MSG : CacheResult.FAIL_WITHOUT_MSG;
         } catch (Throwable e) {
             logError("REMOVE", key, e);
@@ -156,7 +183,7 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
         try {
             if (Objects.nonNull(keys) && !keys.isEmpty()) {
                 final RBatch batch = this.client.createBatch();
-                keys.forEach(key -> batch.getBucket(getCacheKey(key)).deleteAsync());
+                keys.forEach(key -> batch.getBucket(getCacheKey(key), getCodec()).deleteAsync());
                 batch.execute();
             }
             return CacheResult.SUCCESS_WITHOUT_MSG;
@@ -169,8 +196,9 @@ public class RedissonCache<K, V> extends AbstractExternalCache<K, V> {
     @Override
     protected CacheResult do_PUT_IF_ABSENT(final K key, final V value, final long expireAfterWrite, final TimeUnit timeUnit) {
         try {
-            final CacheValueHolder<V> holder = new CacheValueHolder<>(value, timeUnit.toMillis(expireAfterWrite));
-            final boolean success = this.client.getBucket(getCacheKey(key)).trySet(holder, expireAfterWrite, timeUnit);
+            final Duration expire = Duration.ofMillis(timeUnit.toMillis(expireAfterWrite));
+            final CacheValueHolder<V> holder = new CacheValueHolder<>(value, expire.toMillis());
+            final boolean success = this.client.getBucket(getCacheKey(key), getCodec()).setIfAbsent(encoder(holder), expire);
             return success ? CacheResult.SUCCESS_WITHOUT_MSG : CacheResult.EXISTS_WITHOUT_MSG;
         } catch (Throwable e) {
             logError("PUT_IF_ABSENT", key, e);
