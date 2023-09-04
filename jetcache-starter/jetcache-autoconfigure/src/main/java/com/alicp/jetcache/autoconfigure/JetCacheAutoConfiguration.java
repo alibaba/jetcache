650c87b37698c478bc6373b454b4a53315a063ba
zt9788 <zt9788@126.com>
2023-09-04 15:04:09 +0800

    CacheManager destroyMethod

--- jetcache-starter/jetcache-autoconfigure/src/main/java/com/alicp/jetcache/autoconfigure/JetCacheAutoConfiguration.java
+++ jetcache-starter/jetcache-autoconfigure/src/main/java/com/alicp/jetcache/autoconfigure/JetCacheAutoConfiguration.java
@@ -51,9 +51,9 @@ public class JetCacheAutoConfiguration {
                 encoderParser, keyConvertorParser, metricsCallback);
     }
 
-    @Bean(name = "jcCacheManager")
+    @Bean(name = "jcCacheManager",destroyMethod = "close")
     @ConditionalOnMissingBean
-    public CacheManager cacheManager(@Autowired SpringConfigProvider springConfigProvider) {
+    public SimpleCacheManager cacheManager(@Autowired SpringConfigProvider springConfigProvider) {
         SimpleCacheManager cacheManager = new SimpleCacheManager();
         cacheManager.setCacheBuilderTemplate(springConfigProvider.getCacheBuilderTemplate());
         return cacheManager;
